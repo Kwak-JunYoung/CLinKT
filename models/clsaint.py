16fc0834120503da0ff41f33033321859b3fd3f8
Kwak-JunYoung <klop9090@g.skku.edu>
2023-08-28 13:38:04 +0900

    Fix: Modification of overriding structure of clsaint

--- models/clsaint.py
+++ models/clsaint.py
@@ -11,7 +11,7 @@ from .saint import SAINT
 
 class CLSAINT(SAINT):
     def __init__(self, *args, **kwargs):
-        super(SAINT, self).__init__(*args, **kwargs)
+        super(CLSAINT, self).__init__(*args, **kwargs)
         self.args = kwargs
     
     def forward(self, feed_dict):
